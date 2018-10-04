54369814e68f71b8cfd0b130125839025d57a0b3
seely1991 <40604521+seely1991@users.noreply.github.com>
2018-10-04 10:47:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+//fades the input fields when first loaded
 function load() {
 	setTimeout(function () {$("#load").fadeIn(1000);}, 800);
 }
 
+//establishing data for volume to weight conversions using object notation
 class item {
 	constructor(name, weight) {
 		this._name = name;
@@ -20,6 +22,7 @@ class item {
 	}
 }
 
+//creating classes for each ingredient for use with volume to weight conversion
 const flour = new item("flour",125);
 const bakingPowder = new item("baking powder", 192);
 const bakingSoda = new item("baking soda", 6*48);
@@ -40,11 +43,14 @@ const shortening = new item('shortening', 192);
 const darkCornSyrup = new item('dark corn syrup', 328);
 const jimBeamWhiskey = new item('whiskey', 8*24);
 
+//all item objects listed in an array for easy access
 const itemList = [flour, bakingSoda, bakingPowder, brownSugar, butter, breadFlour, cakeFlour, wholeWheatFlour, milk, peanutButter, powderedSugar, salt, sourdoughStarter, sugar, water, cocoaPowder, darkCornSyrup, shortening, jimBeamWhiskey]
 
+//global variable used for making unique id name for each list item
 let listNum = 1;
 
 function showOther () {
+	//if unit measure is something other than options listed (for instance 'pinch' or 'dash' gives the user ability to input own unit (does not work with conversions)
 	if ($("#input").val() === 'other') {
 		$("#other").show();
 		$("#input").hide();
@@ -54,6 +60,7 @@ function showOther () {
 }
 
 $("#hideOther").on('click', function () {
+	//exits out of the 'other' tab in the input field
 	$("#other").hide();
 	$("#input").show();
 	$("#" + this.id).hide();
@@ -61,21 +68,25 @@ $("#hideOther").on('click', function () {
 })
 
 $("#add").on('click', function () {
-
+	//function to add item,
+	//used grep to find item in the itemList array
 	let x = $.grep(itemList, function (n, i) {
 		return n.name === $("#item").val().toLowerCase();
 	})
 
-	
+	//variables to be used in conversions
 	let itm = x[0];
 	let units;
 	let weight = 1;
 	let $input = $("#input").val();
 	let $output = $("#output").val();
+	//sets initial measurement as cups to be converted in the following switch
 	let cups = $("#qty").val();
+	//sets the itm object if it is not found
 	if (x.length === 0) {itm = {name: $("#item").val(), weight: 0,}}
 
 	switch($input){
+			//sets the weight and cups data in accordance with the chosen unit
 			case "cup":
 			break;
 			case "floz":
@@ -96,6 +107,7 @@ $("#add").on('click', function () {
 	}
 
 	switch($("#output").val()){
+			//converts the chosen unit
 			case "g":
 				units = "g";
 				weight = Math.round(weight*$("#qty").val()*itm.weight*100)/100;
@@ -124,7 +136,8 @@ $("#add").on('click', function () {
 				break
 
 	}
-	if ($("#item").val() !== "") {
+	if ($("#item").val()) {
+	//list item to be displayed taking into account potential errors caused by missing information (either on users side or within the volume to weight conversion objects)
 	if (weight === 0) { weight = "Info Not Found"; units = ""}
 	
 	if ($output === "NA" && $input === "") {
@@ -136,11 +149,14 @@ $("#add").on('click', function () {
 	}else{
 		$("#list").append("<div id=\"listItem" + listNum + "\" style=\"display: none\"><div class=\"listText\">" + weight + units + " " + itm.name + "</div><button id=\"" + listNum +"\" type=button id=" + "\"list" + listNum + "\"" + "class=\"xBton delete\"><div class=\"buttonText\">&times;</div></button><br></div>")
 }
+	//testing to see id's of each list item
 	let idName = "listItem" + listNum.toString()
 	console.log(idName)
 	$("#" + idName).slideDown(300);
 
+	//increments the global variable listNum to make a unique id used to delete specific items and have others unaffected
 	listNum ++;
+	//delete items by checking the x in the item div
 	$(".delete").on('click', function () {
 		$("#" + this.id).parent().slideUp(300);
 		setTimeout( function () {$("#" + this.id).parent().remove()},310)
